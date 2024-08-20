632bf90dbcee9ce601770eb3dd8a996b209f824e
TobiasNicEng <tnengleralumno@itec-elmolino.edu.ar>
2024-08-20 18:37:49 -0300

    EventoCheck
    validación de Usuario y contraseña

--- Builder/noname.cpp
+++ Builder/noname.cpp
@@ -7,133 +7,136 @@
 
 #include "noname.h"
 
-///////////////////////////////////////////////////////////////////////////
-
-Login_GUI::Login_GUI( wxWindow* parent, wxWindowID id, const wxString& title, const wxPoint& pos, const wxSize& size, long style ) : wxFrame( parent, id, title, pos, size, style )
+Login_GUI::Login_GUI(wxWindow* parent, wxWindowID id, const wxString& title, const wxPoint& pos, const wxSize& size, long style)
+	: wxFrame(parent, id, title, pos, size, style)
 {
-	this->SetSizeHints( wxDefaultSize, wxDefaultSize );
+	this->SetSizeHints(wxDefaultSize, wxDefaultSize);
 	
-	wxGridBagSizer* gbSizer1;
-	gbSizer1 = new wxGridBagSizer( 0, 0 );
-	gbSizer1->SetFlexibleDirection( wxBOTH );
-	gbSizer1->SetNonFlexibleGrowMode( wxFLEX_GROWMODE_SPECIFIED );
+	wxGridBagSizer* gbSizer1 = new wxGridBagSizer(0, 0);
+	gbSizer1->SetFlexibleDirection(wxBOTH);
+	gbSizer1->SetNonFlexibleGrowMode(wxFLEX_GROWMODE_SPECIFIED);
 	
-	wxBoxSizer* bSizer1;
-	bSizer1 = new wxBoxSizer( wxVERTICAL );
+	wxBoxSizer* bSizer1 = new wxBoxSizer(wxVERTICAL);
 	
-	m_staticText1 = new wxStaticText( this, wxID_ANY, wxT("Ingrese Su Usuario"), wxPoint( 0,0 ), wxSize( 250,20 ), wxALIGN_CENTRE );
-	m_staticText1->Wrap( 250 );
-	bSizer1->Add( m_staticText1, 0, wxALL, 5 );
+	m_staticText1 = new wxStaticText(this, wxID_ANY, ("Ingrese Su Usuario"), wxPoint(0,0), wxSize(250,20), wxALIGN_CENTRE);
+	m_staticText1->Wrap(250);
+	bSizer1->Add(m_staticText1, 0, wxALL, 5);
 	
-	Login_Usuario_ingresado = new wxTextCtrl( this, wxID_ANY, wxT("Escriba Aqui..."), wxPoint( 2,2 ), wxSize( 250,25 ), wxTE_CENTRE );
-	bSizer1->Add( Login_Usuario_ingresado, 0, wxALL, 5 );
+	Login_Usuario_ingresado = new wxTextCtrl(this, wxID_ANY, ("Escriba Aqui..."), wxPoint(2,2), wxSize(250,25), wxTE_CENTRE);
+	bSizer1->Add(Login_Usuario_ingresado, 0, wxALL, 5);
 	
+	gbSizer1->Add(bSizer1, wxGBPosition(7, 30), wxGBSpan(1, 1), wxEXPAND, 5);
 	
-	gbSizer1->Add( bSizer1, wxGBPosition( 7, 30 ), wxGBSpan( 1, 1 ), wxEXPAND, 5 );
+	wxBoxSizer* bSizer11 = new wxBoxSizer(wxVERTICAL);
 	
-	wxBoxSizer* bSizer11;
-	bSizer11 = new wxBoxSizer( wxVERTICAL );
+	m_staticText11 = new wxStaticText(this, wxID_ANY, "Ingrese Su Contraseña", wxPoint(0,2), wxSize(250,20), wxALIGN_CENTRE);
+	m_staticText11->Wrap(250);
+	bSizer11->Add(m_staticText11, 0, wxALL, 5);
 	
-	m_staticText11 = new wxStaticText( this, wxID_ANY, wxT("Ingrese Su ContraseÃ±a"), wxPoint( 0,2 ), wxSize( 250,20 ), wxALIGN_CENTRE );
-	m_staticText11->Wrap( 250 );
-	bSizer11->Add( m_staticText11, 0, wxALL, 5 );
+	Login_Contrasenia_ingresado = new wxTextCtrl(this, wxID_ANY, ("Escriba Aqui..."), wxPoint(1,1), wxSize(250,25), wxTE_CENTRE | wxTE_PASSWORD);
+	bSizer11->Add(Login_Contrasenia_ingresado, 0, wxALL, 5);
 	
-	Login_Contrasenia_ingresado = new wxTextCtrl( this, wxID_ANY, wxT("Escriba Aqui..."), wxPoint( 1,1 ), wxSize( 250,25 ), wxTE_CENTRE );
-	bSizer11->Add( Login_Contrasenia_ingresado, 0, wxALL, 5 );
+	gbSizer1->Add(bSizer11, wxGBPosition(11, 30), wxGBSpan(1, 1), wxEXPAND, 5);
 	
+	B_Ingresar = new wxButton(this, wxID_ANY, ("Ingresar"), wxDefaultPosition, wxDefaultSize, 0);
+	B_Ingresar->SetMinSize(wxSize(250,30));
 	
-	gbSizer1->Add( bSizer11, wxGBPosition( 11, 30 ), wxGBSpan( 1, 1 ), wxEXPAND, 5 );
+	gbSizer1->Add(B_Ingresar, wxGBPosition(15, 30), wxGBSpan(1, 1), wxALL, 5);
 	
-	B_Ingresar = new wxButton( this, wxID_ANY, wxT("Ingresar"), wxDefaultPosition, wxDefaultSize, 0 );
-	B_Ingresar->SetMinSize( wxSize( 250,30 ) );
-	
-	gbSizer1->Add( B_Ingresar, wxGBPosition( 15, 30 ), wxGBSpan( 1, 1 ), wxALL, 5 );
-	
-	
-	this->SetSizer( gbSizer1 );
+	this->SetSizer(gbSizer1);
 	this->Layout();
 	
-	this->Centre( wxBOTH );
+	this->Centre(wxBOTH);
 	
 	// Connect Events
-	B_Ingresar->Connect( wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler( Login_GUI::Login_check ), NULL, this );
+	B_Ingresar->Connect(wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler(Login_GUI::Check), NULL, this);
 }
 
 Login_GUI::~Login_GUI()
 {
 	// Disconnect Events
-	B_Ingresar->Disconnect( wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler( Login_GUI::Login_check ), NULL, this );
-	
+	B_Ingresar->Disconnect(wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler(Login_GUI::Check), NULL, this);
 }
 
-Main_GUI::Main_GUI( wxWindow* parent, wxWindowID id, const wxString& title, const wxPoint& pos, const wxSize& size, long style ) : wxFrame( parent, id, title, pos, size, style )
+void Login_GUI::Check(wxCommandEvent& event)
 {
-	this->SetSizeHints( wxDefaultSize, wxDefaultSize );
+	// Obtener los valores ingresados por el usuario
+	wxString usuario = Login_Usuario_ingresado->GetValue();
+	wxString contrasenia = Login_Contrasenia_ingresado->GetValue();
 	
-	wxFlexGridSizer* fgSizer2;
-	fgSizer2 = new wxFlexGridSizer( 1, 1, 0, 0 );
-	fgSizer2->SetFlexibleDirection( wxBOTH );
-	fgSizer2->SetNonFlexibleGrowMode( wxFLEX_GROWMODE_SPECIFIED );
+	// Validación simple (esto es solo un ejemplo, en un caso real debes encriptar y verificar con una base de datos)
+	if (usuario == "admin" && contrasenia == "password")
+	{
+		wxMessageBox("¡Ingreso exitoso!", "Login", wxOK | wxICON_INFORMATION);
+		// Aquí puedes crear y mostrar la ventana principal
+		Main_GUI* mainWindow = new Main_GUI(NULL);
+		mainWindow->Show();
+		this->Close();  // Cierra la ventana de login
+	}
+	else
+	{
+		wxMessageBox("Usuario o contraseña incorrectos", "Error", wxOK | wxICON_ERROR);
+	}
+}
+
+// Implementación de Main_GUI
+
+Main_GUI::Main_GUI(wxWindow* parent, wxWindowID id, const wxString& title, const wxPoint& pos, const wxSize& size, long style)
+	: wxFrame(parent, id, title, pos, size, style)
+{
+	this->SetSizeHints(wxDefaultSize, wxDefaultSize);
 	
-	wxBoxSizer* bSizer4;
-	bSizer4 = new wxBoxSizer( wxVERTICAL );
+	wxFlexGridSizer* fgSizer2 = new wxFlexGridSizer(1, 1, 0, 0);
+	fgSizer2->SetFlexibleDirection(wxBOTH);
+	fgSizer2->SetNonFlexibleGrowMode(wxFLEX_GROWMODE_SPECIFIED);
 	
-	wxBoxSizer* bSizer6;
-	bSizer6 = new wxBoxSizer( wxHORIZONTAL );
+	wxBoxSizer* bSizer4 = new wxBoxSizer(wxVERTICAL);
 	
-	wxBoxSizer* bSizer5;
-	bSizer5 = new wxBoxSizer( wxHORIZONTAL );
+	wxBoxSizer* bSizer6 = new wxBoxSizer(wxHORIZONTAL);
 	
-	b_busqueda = new wxButton( this, wxID_ANY, wxT("Buscar Libro"), wxDefaultPosition, wxDefaultSize, 0 );
-	bSizer5->Add( b_busqueda, 0, wxALL, 5 );
+	wxBoxSizer* bSizer5 = new wxBoxSizer(wxHORIZONTAL);
 	
-	b_detalles = new wxButton( this, wxID_ANY, wxT("Ver Detalles"), wxDefaultPosition, wxDefaultSize, 0 );
-	bSizer5->Add( b_detalles, 0, wxALL, 5 );
+	b_busqueda = new wxButton(this, wxID_ANY, ("Buscar Libro"), wxDefaultPosition, wxDefaultSize, 0);
+	bSizer5->Add(b_busqueda, 0, wxALL, 5);
 	
-	b_agregar_fav = new wxButton( this, wxID_ANY, wxT("AÃ±adir a Favoritos"), wxDefaultPosition, wxDefaultSize, 0 );
-	bSizer5->Add( b_agregar_fav, 0, wxALL, 5 );
+	b_detalles = new wxButton(this, wxID_ANY, ("Ver Detalles"), wxDefaultPosition, wxDefaultSize, 0);
+	bSizer5->Add(b_detalles, 0, wxALL, 5);
 	
+	b_agregar_fav = new wxButton(this, wxID_ANY, ("Añadir a Favoritos"), wxDefaultPosition, wxDefaultSize, 0);
+	bSizer5->Add(b_agregar_fav, 0, wxALL, 5);
 	
-	bSizer6->Add( bSizer5, 1, wxEXPAND, 5 );
+	bSizer6->Add(bSizer5, 1, wxEXPAND, 5);
+	bSizer4->Add(bSizer6, 1, wxEXPAND, 5);
 	
+	wxBoxSizer* bSizer9 = new wxBoxSizer(wxVERTICAL);
 	
-	bSizer4->Add( bSizer6, 1, wxEXPAND, 5 );
+	b_fav = new wxButton(this, wxID_ANY, ("Ver Lista de Favoritos"), wxPoint(-1,-1), wxDefaultSize, wxBU_EXACTFIT);
+	bSizer9->Add(b_fav, 0, wxALL, 5);
 	
-	wxBoxSizer* bSizer9;
-	bSizer9 = new wxBoxSizer( wxVERTICAL );
+	bSizer4->Add(bSizer9, 1, wxEXPAND, 5);
 	
-	b_fav = new wxButton( this, wxID_ANY, wxT("Ver Lista de Favoritos"), wxPoint( -1,-1 ), wxDefaultSize, wxBU_EXACTFIT );
-	bSizer9->Add( b_fav, 0, wxALL, 5 );
+	wxBoxSizer* bSizer7 = new wxBoxSizer(wxHORIZONTAL);
 	
+	b_aniadir = new wxButton(this, wxID_ANY, ("Añadir Libro"), wxDefaultPosition, wxDefaultSize, 0);
+	bSizer7->Add(b_aniadir, 0, wxALL, 5);
 	
-	bSizer4->Add( bSizer9, 1, wxEXPAND, 5 );
+	b_modificar = new wxButton(this, wxID_ANY, ("Modificar Libro"), wxDefaultPosition, wxDefaultSize, 0);
+	bSizer7->Add(b_modificar, 0, wxALL, 5);
 	
-	wxBoxSizer* bSizer7;
-	bSizer7 = new wxBoxSizer( wxHORIZONTAL );
+	b_borrar = new wxButton(this, wxID_ANY, ("Borrar Libro"), wxDefaultPosition, wxDefaultSize, 0);
+	bSizer7->Add(b_borrar, 0, wxALL, 5);
 	
-	b_aniadir = new wxButton( this, wxID_ANY, wxT("AÃ±adir Libro"), wxDefaultPosition, wxDefaultSize, 0 );
-	bSizer7->Add( b_aniadir, 0, wxALL, 5 );
+	bSizer4->Add(bSizer7, 1, wxEXPAND, 5);
 	
-	b_modificar = new wxButton( this, wxID_ANY, wxT("Modificar Libro"), wxDefaultPosition, wxDefaultSize, 0 );
-	bSizer7->Add( b_modificar, 0, wxALL, 5 );
+	m_listBox1 = new wxListBox(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, 0, NULL, 0);
+	bSizer4->Add(m_listBox1, 5, wxALL|wxEXPAND, 5);
 	
-	b_borrar = new wxButton( this, wxID_ANY, wxT("Borrar Libro"), wxDefaultPosition, wxDefaultSize, 0 );
-	bSizer7->Add( b_borrar, 0, wxALL, 5 );
+	fgSizer2->Add(bSizer4, 1, wxEXPAND, 5);
 	
-	
-	bSizer4->Add( bSizer7, 1, wxEXPAND, 5 );
-	
-	m_listBox1 = new wxListBox( this, wxID_ANY, wxDefaultPosition, wxSize( 480,500 ), 0, NULL, 0 ); 
-	bSizer4->Add( m_listBox1, 0, wxALL, 5 );
-	
-	
-	fgSizer2->Add( bSizer4, 1, wxEXPAND, 5 );
-	
-	
-	this->SetSizer( fgSizer2 );
+	this->SetSizer(fgSizer2);
 	this->Layout();
 	
-	this->Centre( wxBOTH );
+	this->Centre(wxBOTH);
 }
 
 Main_GUI::~Main_GUI()
